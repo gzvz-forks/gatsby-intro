35133f9a544e3ab33183c83e1020da5365221845
gzuzmark <alonso.m144@.com>
2020-04-24 22:56:26 -0500

    Add blog posts listing
     Generate posts pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,23 @@ module.exports = {
     title: 'Jesus Marquez website',
     description: 'Site to show blog and personal information',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
